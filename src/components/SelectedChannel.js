1c47b7a7bbd133227358fb448d6d4e2f7918299c
Su988 <suyash988@gmail.com>
2021-01-27 22:05:41 -0500

    sidebar children SelectedChannel continue

--- src/components/SelectedChannel.js
+++ src/components/SelectedChannel.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import { useParams } from 'react-router-dom';
+import Sidebar from './Sidebar';
 
 export default function SelectedChannel() {
   const { channelId } = useParams();
   console.log(channelId);
   return (
-    <>
+    <Sidebar>
       <div>Selected channel</div>
-    </>
+    </Sidebar>
   );
 }
--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,5 +1,10 @@
 import React from 'react';
 
 export default function Sidebar({ children }) {
-  return <div className='sidebar'>{children}</div>;
+  return (
+    <div className='sidebar'>
+      <h4>hello from sidebar</h4>
+      {children}
+    </div>
+  );
 }
